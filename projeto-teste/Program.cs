a1060f74b4468f0022e7b65206c38f72e7d80b7e
Alexandre Lessa <alessa.emcomum@gmail.com>
2020-12-19 21:27:43 -0300

    Acrescentada a mensagem desejado bom dia

--- projeto-teste/Program.cs
+++ projeto-teste/Program.cs
@@ -6,9 +6,9 @@ namespace projeto_teste
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Olá mundo!");
-            Console.WriteLine("Bom dia!");
+            Console.WriteLine("Olá mundo!");            
             Console.WriteLine("Como você está hoje?");
+            Console.WriteLine("Tenha um bom dia");
         }
     }
 }
